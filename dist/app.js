f6257b15a707ec3a4143211def4e5bca506e6992
wilmansirit <wsirit@gmail.com>
2020-05-28 18:38:12 -0400

    Remove the /dist folder

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var menuPrincipal = [
-    { plato_principal: "caraotas", postre: "Dulce de lechoza" },
-    { plato_principal: "Carne mechada", postre: "Dulce de leche" },
-    { plato_principal: "arroz", postre: "tiramisu" },
-    { plato_principal: "tajadas", postre: "duraznos" },
-    { plato_principal: "Sopa", postre: "Dulce de fresas" }
-];
-console.log(typeof menuPrincipal);
